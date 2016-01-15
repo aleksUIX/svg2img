6833c3e00dafe1120b02840d6f1aac2e677e5a5c
aleksander <aleksander@uixlimited.com>
2016-01-15 16:53:47 +0000

    setting up karma wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var Server = require('karma').Server;
 
 
 gulp.task('bundle-js', function() {
@@ -10,5 +11,12 @@ gulp.task('bundle-js', function() {
     .pipe(gulp.dest('dist'));
 });
 
+
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
 gulp.task('default', ['bundle-js']);
