f9463ff4194a89878d215271a300608e67651f4a
aleksander <aleksander@uixlimited.com>
2016-01-19 17:24:50 +0000

    create canvas comments

--- src/modules/CreateCanvas.js
+++ src/modules/CreateCanvas.js
@@ -3,14 +3,20 @@ module.exports = function CreateCanvas() {
   var canvasArray = [];
 
   function createCanvas(svg) {
-    debugger;
+    // wrapper for all canvas elements that will be exported
+    var canvasWrapper = document.createElement('div');
 
+    // create each canvas on the document fragment
     for (var i = 0; i < svg.length; i++) {
       var canvasPartial = document.createElement('canvas');
-      $body.appendChild(canvasPartial);
+      canvasWrapper.appendChild(canvasPartial);
       canvasArray.push(canvasPartial);
     }
 
+    // append canvas elements to the DOM
+    $body.appendChild(canvasWrapper);
+
+    // this method will return canvas wrapper for future work
     return canvasArray;
   }
 
