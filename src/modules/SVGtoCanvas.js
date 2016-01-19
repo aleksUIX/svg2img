49cb00be78e4fdec9d0fc244ca0a20871ba3c79c
aleksander <aleksander@uixlimited.com>
2016-01-19 15:09:28 +0000

    image data

--- src/modules/SVGtoCanvas.js
+++ src/modules/SVGtoCanvas.js
@@ -2,10 +2,14 @@ var canvg = require('../libs/canvg');
 
 
 function SVGtoCanvas() {
-  
+
   return function(args) {
+    var imgData;
+
+    canvg(args.canvasDest, args.svgSrc.innerHTML);
+    imgData = args.canvasDest.toDataURL();
+    console.log(imgData);
   }
-  //canvg(document.getElementById('drawingArea'), '<svg>...</svg>')
 }
 
 
